cbbbbdadbc842f37384f7d184c07e2abda9c31e1
jula1717 <julaplocienniczak@gmail.com>
2022-01-02 22:19:17 +0100

    mvc

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+//takie nazwy do connecta
+    //Database params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');//
+    define('DB_NAME', 'biblioteka');
+    
+    define('APPROOT',dirname(dirname(__FILE__)));
+    define('URLROOT','http://localhost/BibliotekaInternetowa');
+    define('SITENAME','Biblioteka'); //do zmiany
+
+?>
---
+++ app/controllers/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+class Index extends Controller {
+    public function __construct()
+    {
+      
+    }
+    public function index(){
+        
+         $this->view('index');
+
+    }
+}
+?>
