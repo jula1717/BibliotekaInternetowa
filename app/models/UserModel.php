19533471736ecea7733006793a31e703f489199e
jula1717 <julaplocienniczak@gmail.com>
2022-01-03 12:32:40 +0100

    merge readers and employees into users

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -6,20 +6,21 @@
             $this->db = new Database;
         }
         public function getUserByEmail($email){
-            $query='SELECT * FROM "Czytelnicy" WHERE email = :email';
+            $query='SELECT * FROM "Uzytkownicy" WHERE email = :email';
             $this->db->query($query);
             $this->db->bind(':email',$email);
             $result=$this->db->single();
             return $result;
         }
         public function createUser(){
-            $query='INSERT INTO "Czytelnicy" (email, haslo, telefon)
-            VALUES (:email, :password, :phone);';
+            $query='INSERT INTO "Uzytkownicy" (email, haslo, telefon,typ_konta)
+            VALUES (:email, :password, :phone, :typ_konta);';
             $this->db->query($query);
             $this->db->bind(':email',$_POST['email']);
             $hashedPassword=password_hash($_POST['password'],PASSWORD_DEFAULT);
             $this->db->bind(':password',$hashedPassword);
             $this->db->bind(':phone',$_POST['phone']);
+            $this->db->bind(':typ_konta','czytelnik');
             $this->db->execute();
         }
     }
