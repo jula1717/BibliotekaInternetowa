c8fba164cc4157facb0d2989ef12429bdbcbc5bd
jula1717 <julaplocienniczak@gmail.com>
2022-01-03 01:22:11 +0100

    Index.php clear

--- app/controllers/Index.php
+++ app/controllers/Index.php
@@ -3,13 +3,11 @@
 class Index extends Controller {
     public function __construct()
     {
-        $this->firstModel = $this->model('FirstModel');
+
     }
     public function index(){
         
          $this->view('index');
-        var_dump($result = $this->firstModel->getAutorzy());
-
     }
 }
 ?>
