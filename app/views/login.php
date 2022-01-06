9c4857de839bc7305fed02d1cba71ce147907eca
jula1717 <julaplocienniczak@gmail.com>
2022-01-06 13:18:24 +0100

    the appearance of the login and register form

--- app/views/login.php
+++ app/views/login.php
@@ -1,19 +1,49 @@
+<link rel="stylesheet" href="<?php echo URLROOT;?>/css/logreg.css" /></head>
 <?php //todo
             if (isset($_GET['registerSuccessful'])) 
             echo 'Rejestracja przebiegła pomyślnie!<br>'
 ?>
-    <form class ="login" action="<?php echo URLROOT."/login/tryToLogin"?>" method="post">
+<div class ="logreg">
+    <form action="<?php echo URLROOT."/login/tryToLogin"?>" method="post">
 
-        <input type="email" <?php if(isset($_SESSION['errorEmptyLogin'])||isset($_SESSION['errorBadAuthorize']))echo "class='error'";?> id="email" name="email" placeholder="E-mail"><br>
-        <input type="password" <?php if(isset($_SESSION['errorEmptyLogin'])||isset($_SESSION['errorBadAuthorize']))echo "class='error'";?> id="password" name="password" placeholder="Hasło"><br>
-        <input type="submit" value="Zaloguj">
+        <input type="email" <?php if(isset($_SESSION['errorIncompleteLoginData'])||isset($_SESSION['errorInvalidPassword']))echo "class='error'";?> id="email" name="email" placeholder="E-mail" 
+                style="width: 170px;
+                background-color: #bfe8e3 !important;
+                color: #269ca3 !important;
+                font-size: 15px;
+                padding: 10px;
+                box-sizing: border-box;
+                margin-top: 5px;
+                border: 2px solid #a5ded7 !important;">
+    <br>
+        <input type="password" <?php if(isset($_SESSION['errorIncompleteLoginData'])||isset($_SESSION['errorInvalidPassword']))echo "class='error'";?> id="password" name="password" placeholder="Hasło"
+                style="width: 170px;
+                background-color: #bfe8e3 !important;
+                color: #269ca3 !important;
+                font-size: 15px;
+                padding: 10px;
+                box-sizing: border-box;
+                margin-top: 5px;
+                border: 2px solid #a5ded7 !important;">
+    <br>
+        <input type="submit" value="Zaloguj"
+                style="    width: 170px;
+                background-color: #84dbd1;
+                font-size: 20px;
+                color: white;
+                padding: 15px 10px;
+                margin-top: 10px;
+                border: none;
+                border-radius: 10px;
+                cursor: pointer;
+                letter-spacing: 2px;">
     </form>
    
     <?php
-    if (isset($_SESSION['errorEmptyLogin']))echo $_SESSION['errorEmptyLogin'];
-    if (isset($_SESSION['errorBadAuthorize']))echo $_SESSION['errorBadAuthorize'];
+    if (isset($_SESSION['errorIncompleteLoginData']))echo $_SESSION['errorIncompleteLoginData'];
+    if (isset($_SESSION['errorInvalidPassword']))echo $_SESSION['errorInvalidPassword'];
     ?>
      <br>
     Jeśli nie masz jeszcze założonego konta<br>
     <a href="<?php echo URLROOT."/register"?>">kliknij tutaj</a>
-  
+</div>
--- app/views/register.php
+++ app/views/register.php
@@ -1,17 +1,19 @@
+<link rel="stylesheet" href="<?php echo URLROOT;?>/css/logreg.css" /></head>
 <body>
-    <form class ="register" action="<?php echo URLROOT."/register/registerUser"?>" method="post">
-        <input type="email" <?php if(isset($_SESSION['errorEmptyRegister'])||isset($_SESSION['errorTakenRegister']))echo "class='error'";?> id="email" name="email" placeholder="E-mail" required><br>
-        <input type="password" <?php if(isset($_SESSION['errorEmptyRegister'])||isset($_SESSION['errorTakenRegister']))echo "class='error'";?> id="password" name="password" placeholder="Hasło" required><br>
-        <input type="password" <?php if(isset($_SESSION['errorEmptyRegister'])||isset($_SESSION['errorTakenRegister']))echo "class='error'";?> id="confirmPassword" name="confirmPassword" placeholder="Powtórz hasło" required><br>
-        <input type="text" <?php if(isset($_SESSION['errorEmptyRegister'])||isset($_SESSION['errorTakenRegister']))echo "class='error'";?> id="phone" name="phone" placeholder="Telefon" required><br>
+<div class ="logreg">
+    <form  action="<?php echo URLROOT."/register/registerUser"?>" method="post">
+        <input type="email" <?php if(isset($_SESSION['errorIncompleteRegisterData'])||isset($_SESSION['errorDataAlreadyExists']))echo "class='error'";?> id="email" name="email" placeholder="E-mail" required><br>
+        <input type="password" <?php if(isset($_SESSION['errorIncompleteRegisterData'])||isset($_SESSION['errorDataAlreadyExists']))echo "class='error'";?> id="password" name="password" placeholder="Hasło" required><br>
+        <input type="password" <?php if(isset($_SESSION['errorIncompleteRegisterData'])||isset($_SESSION['errorDataAlreadyExists']))echo "class='error'";?> id="confirmPassword" name="confirmPassword" placeholder="Powtórz hasło" required><br>
+        <input type="text" <?php if(isset($_SESSION['errorIncompleteRegisterData'])||isset($_SESSION['errorDataAlreadyExists']))echo "class='error'";?> id="phone" name="phone" placeholder="Telefon" required><br>
         <input type="submit" value="Zarejestruj">
     </form>
     <?php
-    if (isset($_SESSION['errorEmptyRegister']))echo $_SESSION['errorEmptyRegister'];
-    if (isset($_SESSION['errorTakenRegister']))echo $_SESSION['errorTakenRegister'];
+    if (isset($_SESSION['errorIncompleteRegisterData']))echo $_SESSION['errorIncompleteRegisterData'];
+    if (isset($_SESSION['errorDataAlreadyExists']))echo $_SESSION['errorDataAlreadyExists'];
     ?>
     Jeśli masz już założone konto<br>
     <a href="<?php echo URLROOT."/login"?>">kliknij tutaj</a>
-    
+</div>    
     
 </body>
