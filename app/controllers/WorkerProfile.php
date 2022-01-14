f3364d1e184d644fc30a6b6ce5dabbaf62cd4399
jula1717 <julaplocienniczak@gmail.com>
2022-01-14 11:43:34 +0100

    log out

--- app/controllers/WorkerProfile.php
+++ app/controllers/WorkerProfile.php
@@ -3,11 +3,26 @@
 class WorkerProfile extends Controller {
     public function __construct()
     {
+       //czemu wchodzi w ifa jak jest typ administrator lub pracownik??? 
 
+        // if (($_SESSION['userData']->typ_konta!="administrator")||($_SESSION['userData']->typ_konta!="pracownik")) 
+        // {
+        //     echo "typ: ".$_SESSION['userData']->typ_konta;
+        //         exit();
+            
+        //     // header("Location: " . URLROOT . "/login" );
+        //     // exit();
+        // }
     }
     public function index(){
         
          $this->view('workerProfile');
+    }
+
+    public function logout(){
+        session_unset();
+        header("Location: ".URLROOT . "/login");
+        exit();
     }
 }
 ?>
