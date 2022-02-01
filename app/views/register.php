8b6d9a67b9a1a609b54891c167d5f6f466a9223e
jula1717 <julaplocienniczak@gmail.com>
2022-02-01 12:22:06 +0100

    icon search tips in register

--- app/views/register.php
+++ app/views/register.php
@@ -1,23 +1,29 @@
-<link rel="stylesheet" href="<?php echo URLROOT;?>/css/logreg.css" /></head>
+<link rel="stylesheet" href="<?php echo URLROOT;?>/css/logreg.css?v=101" />
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/fontello/fontello.css?v=101" />
+</head>
 <body>
 <div class ="logreg">
     <form  action="<?php echo URLROOT."/register/registerUser"?>" method="post">
         <input type="email" <?php if(isset($_SESSION['errorIncompleteRegisterData'])||isset($_SESSION['errorDataAlreadyExists']))echo "class='error'";?> id="email" name="email" placeholder="E-mail" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}" maxlength="255" required>
+        <i class="icon-search help" title="Wprowadź email."></i>
         <?php
         if (isset($_SESSION['errorWrongEmailSize']))echo $_SESSION['errorWrongEmailSize'];
         ?>
         <br>
         <input type="password" <?php if(isset($_SESSION['errorIncompleteRegisterData'])||isset($_SESSION['errorDataAlreadyExists']))echo "class='error'";?> id="password" name="password" placeholder="Hasło" minlength="8" maxlength="255" required> 
+        <i class="icon-search help" title="Wprowadź hasło(minimum 8 znaków)."></i>
         <?php
         if (isset($_SESSION['errorWrongPasswordSize']))echo $_SESSION['errorWrongPasswordSize'];
         ?>
         <br>
         <input type="password" <?php if(isset($_SESSION['errorIncompleteRegisterData'])||isset($_SESSION['errorDataAlreadyExists']))echo "class='error'";?> id="confirmPassword" name="confirmPassword" placeholder="Powtórz hasło" required minlength="8" maxlength="255">
-        <?php
+        <i class="icon-search help" title="Wprowadź hasło ponownie(minimum 8 znaków, podane hasła muszą się zgadzać)."></i>
+       <?php
         if (isset($_SESSION['errorWrongConfirmPasswordSize']))echo $_SESSION['errorWrongConfirmPasswordSize'];
         ?>
         <br>
         <input type="tel" <?php if(isset($_SESSION['errorIncompleteRegisterData'])||isset($_SESSION['errorDataAlreadyExists']))echo "class='error'";?> id="phone" name="phone" pattern="[0-9]{9}" placeholder="Telefon" minlength="9" maxlength="9" required>
+        <i class="icon-search help" title="Wprowadź numer telefonu jako 9 kolejnych cyfr (przykład 123456789)."></i>
         <?php
         if (isset($_SESSION['errorWrongPhoneSize']))echo $_SESSION['errorWrongPhoneSize'];
         ?>
